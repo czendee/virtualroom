b60d11a3b5c1208d4bce5a41058462061303fe0b
Isaiah <anyiminene@gmail.com>
2020-12-12 03:18:06 -0800

    test4 keyframe for socials

--- src/scenes/LinkTree/assets/constants.ts
+++ src/scenes/LinkTree/assets/constants.ts
@@ -48,7 +48,7 @@ export const keyframes: Keyframe[] = [
   {
     position: linkPositions[0].p
       .clone()
-      .add(new Vector3(-(DIST + 4), 0, -2))
+      .add(new Vector3(-(DIST + 5), 0, -2))
       .multiplyScalar(1 / SCALE),
     label: "socials",
   },
